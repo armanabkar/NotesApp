52381d706e7edf3ad87a31ad0191a2595f033f72
Arman Abkar <armanabkar@gmail.com>
2021-07-04 12:11:59 +0430

    add Core Data

--- NotesApp/Controllers/ViewController.swift
+++ NotesApp/Controllers/ViewController.swift
@@ -6,13 +6,16 @@
 //
 
 import UIKit
+import CoreData
 
-class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class ViewController: UIViewController {
     
     @IBOutlet weak var table: UITableView!
     @IBOutlet weak var label: UILabel!
     
-    var notes: [Note] = []
+    var notes = [Note]()
+    
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,6 +24,13 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         table.dataSource = self
         
         title = "Notes"
+        
+        loadNotes()
+        
+        if !notes.isEmpty {
+            label.isHidden = true
+            table.isHidden = false
+        }
     }
     
     @IBAction func didTapNewNote() {
@@ -33,15 +43,45 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         viewController.completion = { noteTitle, note in
             DispatchQueue.main.async {
                 self.navigationController?.popToRootViewController(animated: true)
-                let newNote = Note(title: noteTitle, note: note)
+                let newNote = Note(context: self.context)
+                newNote.title = noteTitle
+                newNote.note = note
                 self.notes.append(newNote)
                 self.label.isHidden = true
                 self.table.isHidden = false
-                self.table.reloadData()
+                self.saveNotes()
             }
         }
         navigationController?.pushViewController(viewController, animated: true)
     }
+    
+    func saveNotes() {
+        do {
+            try context.save()
+        } catch {
+            print("Error saving notes \(error)")
+        }
+        
+        self.table.reloadData()
+    }
+    
+    func loadNotes() {
+        let request : NSFetchRequest<Note> = Note.fetchRequest()
+        
+        do{
+            notes = try context.fetch(request)
+        } catch {
+            print("Error loading notes \(error)")
+        }
+        
+        table.reloadData()
+    }
+    
+}
+
+// MARK: - Table View
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         notes.count
@@ -65,11 +105,21 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
             let note = self.notes[indexPath.row]
             viewController.navigationItem.largeTitleDisplayMode = .never
             viewController.title = note.title
-            viewController.noteTitle = note.title
-            viewController.note = note.note
+            viewController.noteTitle = note.title ?? ""
+            viewController.note = note.note ?? ""
             self.navigationController?.pushViewController(viewController, animated: true)
         }
     }
     
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            let commit = notes[indexPath.row]
+            context.delete(commit)
+            notes.remove(at: indexPath.row)
+            table.deleteRows(at: [indexPath], with: .fade)
+            
+            self.saveNotes()
+        }
+    }
+    
 }
-
--- NotesApp/Support Files/AppDelegate.swift
+++ NotesApp/Support Files/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import CoreData
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -19,6 +20,28 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
     
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+    }
+    
+    lazy var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "Note")
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container
+    }()
+    
+    func saveContext () {
+        let context = persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
     }
     
 }
